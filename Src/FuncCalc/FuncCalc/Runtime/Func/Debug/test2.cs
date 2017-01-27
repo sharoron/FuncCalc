22dc1268f91319f63514eb7fb5f2550ea168150c
sharoron <kyoko@yryr.me>
2017-01-27 21:06:21 +0900

    test2の追加

---
+++ Src/FuncCalc/FuncCalc/Runtime/Func/Debug/test2.cs
@@ -0,0 +1,42 @@
+﻿using FuncCalc.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuncCalc.Expression;
+
+namespace FuncCalc.Runtime.Func
+{
+    public class Test2 : IFunction
+    {
+        public override string Name
+        {
+            get
+            {
+                return "test2";
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                return "5をかける関数です";
+            }
+        }
+        public override ExpressionType[] Parameter
+        {
+            get { return new ExpressionType[] { ExpressionType.Number }; }
+        }
+        public override ExpressionType ReturnType
+        {
+            get
+            {
+                return ExpressionType.Number;
+            }
+        }
+        public override INumber Execute(RuntimeData runtime, params INumber[] parameters) {
+            return parameters[0].Multiple(runtime, Number.New(5));
+        }
+    }
+}
